bae58903004b50dbb548715597af8e267726d342
Jeremi Leveille <you@example.com>
2022-10-30 23:01:17 -0400

    Set a basic overlay menu to implement database functionalities

--- t-course-page-app.js
+++ t-course-page-app.js
@@ -32,47 +32,110 @@ const navItemFiles = document.querySelector('#overlay-nav-item-files');
 
 overlayNavItems = [navItemAssignment, navItemAnnouncements, navItemGrades, navItemFiles];
 
+const assigmentOptions = document.getElementById('overlay-assignment-options')
+const assignmentLayout = document.getElementById('assignments-layout');
+const announcementLayout = document.getElementById('announcements-layout');
+const gradesLayout = document.getElementById('grades-layout');
+const filesLayout = document.getElementById('new-file-layout');
+
 navItemAssignment.addEventListener('click', function () {
-    const assignmentLayout = document.getElementById('assignment-layout');
-    assignmentLayout.style.display = "flex";
+    assigmentOptions.style.display = "flex";
+
+    assignmentLayout.style.display = "none"
+    announcementLayout.style.display = "none";
+    gradesLayout.style.display = "none";
+    filesLayout.style.display = "none";
+
 })
 
 navItemAnnouncements.addEventListener('click', function () {
-    const announcementLayout = document.getElementById('announcement-layout');
+
+
     announcementLayout.style.display = "flex";
+
+    assigmentOptions.style.display = "none";
+    assignmentLayout.style.display = "none";
+    gradesLayout.style.display = "none";
+    filesLayout.style.display = "none";
 })
 
 navItemGrades.addEventListener('click', function () {
-    const gradesLayout = document.getElementById('grades-layout');
+
     gradesLayout.style.display = "flex";
+
+    assigmentOptions.style.display = "none";
+    assignmentLayout.style.display = "none";
+    announcementLayout.style.display = "none";
+    filesLayout.style.display = "none";
 })
 
 navItemFiles.addEventListener('click', function () {
-    const filesLayout = document.getElementById('files-layout');
+
     filesLayout.style.display = "flex";
+
+    assigmentOptions.style.display = "none";
+    assignmentLayout.style.display = "none";
+    announcementLayout.style.display = "none";
+    gradesLayout.style.display = "none";
 })
 
 
+const inOverlayNewAssignment = document.getElementById("overlay-new-assignment-btn");
+inOverlayNewAssignment.addEventListener('click', function () {
+    assignmentLayout.style.display = "flex"
+
+    assigmentOptions.style.display = "none";
+    announcementLayout.style.display = "none";
+    gradesLayout.style.display = "none";
+    filesLayout.style.display = "none";
+})
+
 
 const attachFileBtns = [];
+
 for (let i = 0; i < AllIcons.length; i++) {
     if (AllIcons[i].innerText == "attach_file_add") {
         attachFileBtns.push(AllIcons[i]);
         attachFileBtns[attachFileBtns.length - 1].addEventListener('click', function () {
             overlayContainer.style.display = "flex";
             overlayPanel.style.display = "flex";
+            filesLayout.style.display = "flex";
+
         })
     } else if (AllIcons[i].innerText == "post_add") {
         AllIcons[i].addEventListener('click', function () {
             overlayContainer.style.display = "flex";
             overlayPanel.style.display = "flex";
+            assigmentOptions.style.display = "flex";
+        })
+    } else if (AllIcons[i].innerText == "campaign") {
+        AllIcons[i].addEventListener('click', function () {
+            overlayContainer.style.display = "flex";
+            overlayPanel.style.display = "flex";
+            announcementLayout.style.display = "flex";
         })
     } else if (AllIcons[i].innerText == "close") {
         AllIcons[i].addEventListener('click', function () {
             overlayContainer.style.display = "none";
             overlayPanel.style.display = "none";
+
+            assignmentLayout.style.display = "none"
+            assigmentOptions.style.display = "none";
+            announcementLayout.style.display = "none";
+            gradesLayout.style.display = "none";
+            filesLayout.style.display = "none";
         })
     }
 }
 
 console.log(attachFileBtns);
+
+
+/*
+const gradesInput = document.querySelector('#grade-nb');
+
+gradesInput.addEventListener('click', (event) => {
+    gradesInput.disabled = false;
+});
+
+*/
