8ba3d4570222c05ac14e0219ff518c016a7a6734
jdkentnb <44985266+jdkentnb@users.noreply.github.com>
2022-11-11 00:28:30 -0500

    combining teacher and student dashboards

--- teacher-dashboard.js
+++ teacher-dashboard.js
@@ -40,30 +40,58 @@ const size = courselistSnap.length;
 //TODO: Have each course card direct to that course's page.
 
 //Iterate over courseList and create a course card for each course
-for (var i = 0; i < size; i++) {
-  //Get the course name at index i
-  var courseRef = courselistSnap[i];
-  var courseSnap = await getDoc(courseRef);
-  var courseName = courseSnap.get("name");
+if(localStorage.getItem("isProf")){
 
-  //Add course card div inside the main_panel
-  let div = document.createElement('div');
-  div.className = 'mp_course';
-  div.setAttribute("id", "mp_course");
+  //TODO: Make this a function. isProf is a parameter. 
+  //That way, none of the following code isn't duplicated.
+  for (var i = 0; i < size; i++) {
+    //Get the course name at index i
+    var courseRef = courselistSnap[i];
+    var courseSnap = await getDoc(courseRef);
+    var courseName = courseSnap.get("name");
+  
+    //Add course card div inside the main_panel
+    let div = document.createElement('div');
+    div.className = 'mp_course';
+    div.setAttribute("id", "mp_course");
+  
+    div.innerHTML = `
+          <div class="mp_course1">
+  
+          <a class="main-page-a-tag" href="t-course-page.html" 
+              onclick="localStorage.setItem('currentCourse', '`+ courseName + `');" >
+            <h3>` + courseName + `</h3>
+          </a>
+        </div>
+        <div class="mp_course2">
+          <div class="mp_course2_items">Add Assignment</div>
+          <div class="mp_course2_items">Input Marks</div>
+          <div class="mp_course2_items">View Report</div>
+        </div>`;
+  
+    document.getElementById('main_panel').appendChild(div);
+  }
+}
+else{
+  
+  // //Niko's Dashboard div
+  // //TODO: Combine our css
+  // <div class="my-course">
+  //   <div class="courseName-Grade">
+  //       <div class="courseName">
+  //           <h3><a class="courselink" href="student-course-page.html">COMP 248</a></h3>
+  //       </div>
+  //       <div class="courseGrade">
+  //           67.1%
+  //       </div>
+  //   </div>
+  //   <div class="upcoming-deadlines">
+  //       <h3>Upcoming...</h3>
+  //       <div class="deadline">Assignment 1 submission - 1/10/2022</div>
+  //       <div class="deadline">Lab 2 submission - 15/10/2022</div>
+  //       <div class="deadline">Assignment 2 submission - 31/10/2022</div>
+  //   </div>
+  // </div>
 
-  div.innerHTML = `
-        <div class="mp_course1">
 
-        <a class="main-page-a-tag" href="t-course-page.html" 
-            onclick="localStorage.setItem('currentCourse', '`+ courseName + `');" >
-          <h3>` + courseName + `</h3>
-        </a>
-      </div>
-      <div class="mp_course2">
-        <div class="mp_course2_items">Add Assignment</div>
-        <div class="mp_course2_items">Input Marks</div>
-        <div class="mp_course2_items">View Report</div>
-      </div>`;
-
-  document.getElementById('main_panel').appendChild(div);
 }
