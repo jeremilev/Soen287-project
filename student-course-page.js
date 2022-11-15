56a0ada86bcee2cc7b34069d34a05fbde05a5a4e
jdkentnb <44985266+jdkentnb@users.noreply.github.com>
2022-11-14 23:27:18 -0500

    Add yaniss's code to main-correct-copy
    Copied the code from main

---
+++ student-course-page.js
@@ -0,0 +1,97 @@
+import { getGrades } from '/student-course-test.js';
+
+
+const gradesOverlayContainer = document.getElementById('grades-overlay-container');
+console.log(gradesOverlayContainer);
+const gradesOverlayPanel = document.getElementById('grades-overlay-panel');
+console.log(gradesOverlayPanel);
+const gradesOverlayLayout = document.getElementById('grades-overlay-layout'); 
+const viewGradesBtn = document.getElementById("view-grades-btn");
+console.log(viewGradesBtn);
+
+const navItemGrades = document.querySelector('#grades-overlay-nav-item');
+
+const closeBtn = document.getElementById("grades-overlay-close")
+
+viewGradesBtn.addEventListener("click", function() {
+    gradesOverlayContainer.style.display = "flex";
+    gradesOverlayPanel.style.display = "flex";
+    gradesOverlayLayout.style.display = "flex"; 
+})
+closeBtn.addEventListener("click", function() {
+    gradesOverlayContainer.style.display = "none";
+    gradesOverlayPanel.style.display = "none";
+    gradesOverlayLayout.style.display = "none"; 
+
+})
+
+
+const gradesOverlayPanelQuery = document.getElementById('grades-overlay-panel');
+const gradesOverlayLayoutQuery = document.getElementById('grades-overlay-layout');
+
+
+const displayGrades = async function () {
+    var gradesMap = await getGrades();
+
+    for (let i = 0; i < Object.keys(gradesMap).length; i++) {
+        //Create elements to hold data
+        var container = document.createElement('div');
+        
+
+        //Assign data from announcementsMap
+        let grades = gradesMap[Object.keys(gradesMap)[i]];
+
+        //Get data by key name
+        container.innerText = grades['gradeName'] + " - " + grades['gradeValue'] + "% - Weight: " + grades['gradeWeight'];
+        
+
+        //Give them css formatting
+        container.classList.add('grades-container');
+        
+
+
+        //Add elements to the DOM at the right location
+        gradesOverlayPanelQuery.insertBefore(container, gradesOverlayLayoutQuery);
+    }
+}
+
+displayGrades();
+
+const calculateGrade = async function(){
+    var gradesMap = await getGrades();
+    let totalGrade = 0;
+    let sum = 0;
+    for (let i = 0; i < Object.keys(gradesMap).length; i++) {
+        
+        let grades = gradesMap[Object.keys(gradesMap)[i]];
+        let weight = gradesMap[Object.keys(gradesMap)[i]];
+
+        
+
+
+        totalGrade = (grades['gradeValue']*weight['gradeWeight'])/100;
+        sum = sum + totalGrade;
+        
+    }
+
+    
+    
+
+
+    console.log(sum);
+    var container = document.createElement('div');
+    container.innerText = "Overall Grade: " + sum + "%";
+    container.classList.add('overall-grades-container');
+    gradesOverlayPanelQuery.insertBefore(container, gradesOverlayLayoutQuery);
+
+
+
+
+ 
+}
+
+calculateGrade();
+
+
+
+
---
+++ student-course-test.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getFirestore, collection, addDoc, query, where, doc, getDocs, getDoc } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
+import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgFGNCDCuhhPGm8dkQujxuix0VpJbS3N0",
+    authDomain: "soen287-14875.firebaseapp.com",
+    databaseURL: "https://soen287-14875-default-rtdb.firebaseio.com",
+    projectId: "soen287-14875",
+    storageBucket: "soen287-14875.appspot.com",
+    messagingSenderId: "32157055896",
+    appId: "1:32157055896:web:d35e1510c1e470604e49ed"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+export const getGrades = async function () {
+    //Specify path with commas, so you get database/courses/COMP232-A
+    const docRef = doc(db, "users", "aStudent");
+
+    //Get the data from the reference above
+    const docSnap = await getDoc(docRef);
+
+    //If exists
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        //Get the data from that document: IE get the FIELDS DATA
+        try {
+            var grades = docSnap.get("grades");
+        } catch (error) {
+            console.log(e);
+        }
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+    return grades;
+}
