e35d62b1382248e8821f3eef540616c9ded03585
jdkentnb <44985266+jdkentnb@users.noreply.github.com>
2022-12-05 13:49:36 -0500

    Successfully log text file to console

--- admin.js
+++ admin.js
@@ -1,4 +1,22 @@
+
+
+const inputElement = document.getElementById('users-json-file');
+inputElement.addEventListener('change', function() {
+        
+    var fr = new FileReader();
+    fr.onload = function(){
+        console.log(fr.result);
+    }
+        
+    fr.readAsText(this.files[0]);
+})
+  
+
 const createUsersBtn = document.getElementById('btn-create-users');
 createUsersBtn.addEventListener('click', (e) => {
+
+    const selectedFile = document.getElementById('users-json-file').files[0];
     const reader = new FileReader();
+    reader.readAsText(selectedFile);
+    console.log(reader.result);
 })
